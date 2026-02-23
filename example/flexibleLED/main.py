1e4f6c30961bd171fca5dcf13a174b7a8900a724
jin_neuron <yuujinyuujin77@gmail.com>
2026-02-23 17:18:16 +0900

    apply led example to TurtlePico Firmware

--- example/flexibleLED/main.py
+++ example/flexibleLED/main.py
@@ -1,17 +1,14 @@
 import machine
 from machine import PWM, Pin
 import time
-from lib.TurtlePico import Leatherback
 
-RSW = Pin(Leatherback.SW_R, Pin.IN, Pin.PULL_DOWN)
-LSW = Pin(Leatherback.SW_L, Pin.IN, Pin.PULL_DOWN)
+RSW = Pin("SW_R", Pin.IN, Pin.PULL_DOWN)
+LSW = Pin("SW_L", Pin.IN, Pin.PULL_DOWN)
 
-LEDR = Pin(Leatherback.LED_R, Pin.OUT)
-LEDL = Pin(Leatherback.LED_L, Pin.OUT)
-
-i = 0
-status = 0
+LEDR = Pin("LED_R", Pin.OUT)
+LEDL = Pin("LED_L", Pin.OUT)
 
 while True:
-    print("Button R:", RSW.value(), "Button L:", LSW.value())
+    LEDL.value(LSW.value())
+    LEDR.value(RSW.value())
     time.sleep(0.1)
