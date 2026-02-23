916f216b212c71502b5abe4480e635a7eca78deb
jin_neuron <yuujinyuujin77@gmail.com>
2026-02-23 17:49:47 +0900

    apply i2s example to TurtlePico

--- example/I2S_Sound/main.py
+++ example/I2S_Sound/main.py
@@ -3,18 +3,17 @@ from machine import Pin
 from machine import SoftSPI as SPI
 from lib.wavplayer import WavPlayer
 from lib.sdcard import SDCard
-from lib.TurtlePico import Leatherback
 
-cs = Pin(Leatherback.SD_CS)
+cs = Pin("SD_CS")
 
 spi = SPI( baudrate = 25_000_000,
            polarity=0,
            phase=0,
            bits=8,
            firstbit=SPI.MSB,
-           sck  = Pin(Leatherback.SPI_SCK),
-           mosi = Pin(Leatherback.SPI_MOSI),
-           miso = Pin(Leatherback.SPI_MISO))
+           sck  = Pin("SPI_SCK"),
+           mosi = Pin("SPI_MOSI"),
+           miso = Pin("SPI_MISO"))
 
 sd = SDCard(spi, cs)
 sd.init_spi(25_000_000)  # increase SPI bus speed to SD card
@@ -25,10 +24,10 @@ print(list)
 WAV_FILE = "09-Someday-My-Prince-Will-Come.wav"
 
 wp = WavPlayer(
-    id=Leatherback.I2S_ID,
-    sck_pin=Leatherback.I2S_BCLK,
-    ws_pin=Leatherback.I2S_LRCLK,
-    sd_pin=Leatherback.I2S_SDATA,
+    id=0,
+    sck_pin=Pin("I2S_BCLK"),
+    ws_pin=Pin("I2S_LRCLK"),
+    sd_pin=Pin("I2S_SDATA"),
     ibuf=40000,
     volume=-2
 )
